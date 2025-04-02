42d26e9ef64560f1972a9ab11265ef4ad5a02c9e
Benjamin <Benjamin060101@gmail.com>
2025-04-02 12:15:54 +0200

    derhenad

--- AuthService/Program.cs
+++ AuthService/Program.cs
@@ -1,33 +1,42 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string mySecret = Environment.GetEnvironmentVariable("Secret") ?? "none";
-string myIssuer = Environment.GetEnvironmentVariable("Issuer") ?? "none";
+// Konfigurer Vault-klienten
+var vaultAddress = "https://localhost:8201"; // Vault-serverens adresse
+var vaultToken = "00000000-0000-0000-0000-000000000000"; // Root-token fra din Vault
+var authMethod = new TokenAuthMethodInfo(vaultToken);
+var vaultClientSettings = new VaultClientSettings(vaultAddress, authMethod);
+var vaultClient = new VaultClient(vaultClientSettings);
+
+// Hent hemmeligheder fra Vault
+Secret<SecretData> secretData = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync("kv/my-secret");
+string mySecret = secretData.Data.Data["Secret"].ToString();
+string myIssuer = secretData.Data.Data["Issuer"].ToString();
 
 builder.Services
-.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-options.TokenValidationParameters = new TokenValidationParameters()
-{
-ValidateIssuer = true,
-ValidateAudience = true,
-ValidateLifetime = true,
-ValidateIssuerSigningKey = true,
-ValidIssuer = myIssuer, 
-ValidAudience = "http://localhost",
-IssuerSigningKey =
-new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
-};
-});
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = myIssuer,
+            ValidAudience = "http://localhost",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mySecret))
+        };
+    });
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
